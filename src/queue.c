d5079dbfdb8ec1342510eb5068733682a5a81ec3
minh2312130 <2021agl11.truonghoangminh@gmail.com>
2025-04-29 23:26:00 +0700

    Huy's queue

--- src/queue.c
+++ src/queue.c
@@ -2,57 +2,51 @@
 #include <stdlib.h>
 #include "queue.h"
 
-int empty(struct queue_t* q) {
-    if (q == NULL) return 1;
-    return (q->size == 0);
+int empty(struct queue_t * q) {
+        if (q == NULL) return 1;
+	return (q->size == 0);
 }
 
-void enqueue(struct queue_t* q, struct pcb_t* proc) {
-    /* TODO: put a new process to queue [q] */
+void enqueue(struct queue_t * q, struct pcb_t * proc) {
+        /* TODO: put a new process to queue [q] */
     if (q == NULL) {
         fprintf(stderr, "Error: Queue does not exist.\n");
-            return;
+        return;
     }
 
     if (proc == NULL) {
-        fprintf(stderr, "Error: Process does not exist.\n");
+            fprintf(stderr, "Error: Process does not exist.\n");
             return;
-    }
-
-    if (q->size < MAX_QUEUE_SIZE) {
-        q->proc[q->size] = proc;
-        q->size++;
+        }
+    
+    if (q->size < MAX_QUEUE_SIZE){
+        q->proc[q->size] = proc ;
+        q->size=q->size + 1;
     }
 }
 
-struct pcb_t* dequeue(struct queue_t* q) {
-    /* TODO: return a pcb whose priority is the highest
-     * in the queue [q] and remember to remove it from q
-     */
-    if (q->size == 0) {
-        return NULL;
-    }
-    int index = 0;
-#ifdef MLQ_SCHED
+struct pcb_t * dequeue(struct queue_t * q) {
+        /* TODO: return a pcb whose prioprity is the highest*/
 
-    struct pcb_t* retval = q->proc[0];
-    index = 0;
-#else
+        if (q->size == 0) return NULL; // Handle empty queue case
 
-    uint32_t best_prio = q->proc[0]->priority;
-    for (int i = 1; i < q->size; i++) {
-        if (q->proc[i]->priority < best_prio) {
-            best_prio = q->proc[i]->priority;
-            index = i;
+        int max_idx = 0; // Assume first element has the highest priority
+        for (int i = 1; i < q->size; i++) {
+            if (q->proc[i]->prio < q->proc[max_idx]->prio) {
+                max_idx = i; // Update max_idx for higher priority process
+            }
         }
-    }
-    struct pcb_t* retval = q->proc[index];
-#endif
-
-    for (int i = index; i < q->size - 1; i++) {
-        q->proc[i] = q->proc[i + 1];
-    }
-    q->size--;
-    return retval;
+    
+        struct pcb_t *highest_priority_proc = q->proc[max_idx]; // Save process pointer
+    
+        // Shift elements to fill the gap left by the removed process
+        for (int i = max_idx; i < q->size - 1; i++) {
+            q->proc[i] = q->proc[i + 1];
+        }
+    
+        q->size=q->size - 1; // Decrease size to reflect removal
+    
+        return highest_priority_proc; // Return the removed process 
+        //highest_priority_proc -> minimum 
 }
 
