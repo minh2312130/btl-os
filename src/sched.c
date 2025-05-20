0663866b87a40ed327249e49e4b7b01997bc6abd
minh2312130 <2021agl11.truonghoangminh@gmail.com>
2025-05-20 20:34:15 +0700

    update sched
    sched của Huy

--- src/sched.c
+++ src/sched.c
@@ -8,13 +8,6 @@
 static struct queue_t ready_queue;
 static struct queue_t run_queue;
 static pthread_mutex_t queue_lock;
-
-int last_period = 0;        //this variable is used to keep the last_period 
-int get_time_slot();        //this method is called from os.c
-int current_time();         //this method is called from timer.c
-void period_check();        //this method is declared forwarded, use for period checking
-const int sum = 9870;       //this variable is the sum of all integers from 1 to 140 , representing how many slots are there 
-
 static struct queue_t running_list;
 #ifdef MLQ_SCHED
 static struct queue_t mlq_ready_queue[MAX_PRIO];
@@ -57,39 +50,37 @@ struct pcb_t * get_mlq_proc(void) {
 	/*TODO: get a process from PRIORITY [ready_queue].
 	 * Remember to use lock to protect the queue.
 	 * */
-	static int used_slot[MAX_PRIO] = {0}; 
- 
+	
 	pthread_mutex_lock(&queue_lock);
- 
-	for (int prio = 0; prio < MAX_PRIO; prio++) {
-		if (!empty(&mlq_ready_queue[prio])) {
-			if (used_slot[prio] < (MAX_PRIO - prio)) {
-				proc = dequeue(&mlq_ready_queue[prio]);
-				used_slot[prio]++;
-				pthread_mutex_unlock(&queue_lock);
-				return proc;
-			}
+	
+	int cursor;
+	for(cursor = 0;cursor < MAX_PRIO; ++cursor){
+		if ((empty(&mlq_ready_queue[cursor]) == 0) && (slot[cursor] > 0)){
+			proc = dequeue(&mlq_ready_queue[cursor]);
+			slot[cursor] = slot[cursor] - 1;
+			break;
 		}
 	}
-	//neu khong tim duoc proc nao thi reset lai used_slot
-	for (int i = 0; i < MAX_PRIO; i++) {
-		used_slot[i] = 0;
+	
+	if (proc == NULL){
+	  for (int i = 0; i < MAX_PRIO; i ++){
+		mlq_ready_queue[i].size = 0;
+		slot[i] = MAX_PRIO - i; 
+	  }
 	}
- 
-	for (int prio = 0; prio < MAX_PRIO; prio++) {
-		if (!empty(&mlq_ready_queue[prio])) {
-			proc = dequeue(&mlq_ready_queue[prio]);
-			used_slot[prio]++;
-			pthread_mutex_unlock(&queue_lock);
-			return proc;
+	
+	for(cursor = 0;cursor < MAX_PRIO; ++cursor){
+		if ((empty(&mlq_ready_queue[cursor]) == 0) && (slot[cursor] > 0)){
+			proc = dequeue(&mlq_ready_queue[cursor]);
+			slot[cursor] = slot[cursor] - 1;
+			break;
 		}
 	}
- 
+	
 	pthread_mutex_unlock(&queue_lock);
-	return NULL;
+	//return is fine 
+	return proc;	
 }
-
-
 
 void put_mlq_proc(struct pcb_t * proc) {
 	pthread_mutex_lock(&queue_lock);
@@ -127,6 +118,7 @@ void add_proc(struct pcb_t * proc) {
     //os.c call this function
 	return add_mlq_proc(proc);
 }
+
 
 #else
 struct pcb_t * get_proc(void) {
